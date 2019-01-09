3429cbbe5ca153b450e4330d5d24f0e55e26908c
Luc Boussant-roux <luc.boussant-roux@sigmalis.ch>
2019-01-09 21:56:57 +0100

    Ajout de la possibilité d'upload et de delete un file

--- project/app/models.py
+++ project/app/models.py
@@ -8,7 +8,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    posts = db.relationship('Post', backref='author', lazy='dynamic')
+    files = db.relationship('File', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -19,14 +19,14 @@ class User(UserMixin, db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
-class Post(db.Model):
+class File(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    body = db.Column(db.String(140))
+    filename = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
-        return '<Post {}>'.format(self.body)
+        return '<File {}>'.format(self.body)
 
 @login.user_loader
 def load_user(id):
--- project/app/routes.py
+++ project/app/routes.py
@@ -1,16 +1,19 @@
-from app import app
+from app import app, db
+import os
 from flask import render_template, redirect, url_for, flash, request
 from flask_login import current_user, login_user, logout_user, login_required
-from app.models import User
+from app.models import User, File
+from werkzeug import secure_filename
 from werkzeug.urls import url_parse
-from app.forms import LoginForm
+from app.forms import LoginForm, RegistrationForm, DeletionForm
 
 @app.route('/')
 @app.route('/index')
 @login_required
 def index():
-	user = {'username': 'admin'}
-	return render_template('index.html', title="Home", user=user)
+    user = current_user
+    files = File.query.all()
+    return render_template('index.html', title="Home", user=user, files=files)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -33,3 +36,55 @@ def login():
 def logout():
 	logout_user()
 	return redirect(url_for('index'))
+
+@app.route('/upload')
+def upload():
+   return render_template('upload.html')
+	
+@app.route('/uploader', methods = ['POST'])
+def uploader():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(secure_filename(f.filename))
+        user = User.query.filter_by(username="admin").first()
+        newfile = File(filename=f.filename, author=user)
+        db.session.add(newfile)
+        db.session.commit()
+        flash('File uploaded successfully')
+        return render_template('upload.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/delete', methods=['GET', 'POST'])
+def delete():
+    form = DeletionForm()
+    form.filename.choices = [(int(f.id), f.filename) for f in User.query.filter_by(username='admin').first().files]
+    return render_template('delete.html', title='Delete', form=form)
+
+@app.route('/deleter', methods=['POST', 'GET'])
+def deleter():
+    if not current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = DeletionForm()
+    if form.validate_on_submit():
+        f = File.query.filter_by(id=form.filename.data).first()
+        if os.path.exists(f.filename):
+            os.remove(f.filename)
+            db.session.delete(f)
+            db.session.commit()
+            return f.filename + " has been deleted correctly"
+        else:
+            return 'Error'
+    return str(form.errors)
