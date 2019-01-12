d542e205539919619906aa1424387c383394a789
Kozma Viktor <>
2019-01-12 19:16:43 +0100

    table added

--- project/app/__init__.py
+++ project/app/__init__.py
@@ -3,10 +3,19 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
+from flask_fontawesome import FontAwesome
+
 
 app = Flask(__name__)
+Bootstrap(app)
+
+fa = FontAwesome(app)
+
 app.config.from_object(Config)
 db = SQLAlchemy(app)
+app.debug = True
+
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
